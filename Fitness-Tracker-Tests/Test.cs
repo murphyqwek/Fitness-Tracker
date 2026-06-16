11e00d00d11f8a4af636f4cf2b54fc539a8cd72b
murphyqwek <qwekinaft@list.ru>
2026-06-16 21:59:18 +0300

    Add unit test for testing github action

---
+++ Fitness-Tracker-Tests/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fitness_Tracker_Tests
+{
+    public class Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            int expected = 5;
+            // Act
+            int actual = 2 + 3;
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
