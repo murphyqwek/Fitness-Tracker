9999d19416bb8078a185fe37ec8c71c0f4d33058
murphyqwek <qwekinaft@list.ru>
2026-07-06 20:59:29 +0300

    Add basics Entiries to Domain

---
+++ Fintes-Tracker-Domain/Entity/Muscle.cs
@@ -0,0 +1,14 @@
+﻿namespace Fintess_Tracker_Domain.Entity
+{
+    public class Muscle
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Muscle(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ Fintes-Tracker-Domain/Entity/WorkoutSet.cs
@@ -0,0 +1,24 @@
+﻿namespace Fintess_Tracker_Domain.Entity
+{
+    public class WorkoutSet
+    {
+        public Guid Id { get; private set; }
+        public Exercise Exercise { get; private set; }
+        public int Repetitions { get; private set; }
+        public decimal Weight { get; private set; }
+        public int Order { get; private set; }
+
+        public WorkoutSet(Guid id, Exercise exercise, int repetitions, decimal weight, int order)
+        {
+            Id = id;
+            Exercise = exercise;
+            Repetitions = repetitions;
+            Weight = weight;
+            Order = order;
+        }
+
+        public WorkoutSet(Exercise exercise, int repetitions, decimal weight, int order) : this(Guid.NewGuid(), exercise, repetitions, weight, order)
+        {
+        }
+    }
+}
