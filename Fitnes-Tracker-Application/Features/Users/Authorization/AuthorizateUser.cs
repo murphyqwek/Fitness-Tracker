c4c354e39b29842ee6632628bab030d01327f715
murphyqwek <qwekinaft@list.ru>
2026-07-07 15:46:20 +0300

    Add Commands and Handlers for Authorization and Registration users

---
+++ Fitnes-Tracker-Application/Features/Users/Authorization/AuthorizateUser.cs
@@ -0,0 +1,38 @@
+﻿using Fintess_Tracker_Application.Repository.User;
+using MediatR;
+
+namespace Fintess_Tracker_Application.Features.Users.Authorization
+{
+    public record AuthorizateUserCommand(string Login, string Password) : IRequest<Guid>;
+
+    public class AuthorizateUserCommandHandler : IRequestHandler<AuthorizateUserCommand, Guid>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AuthorizateUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Guid> Handle(AuthorizateUserCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _userRepository.GetUserByLoginAsync(request.Login, cancellationToken);
+
+            if (result.IsFailed)
+            {
+                throw new Exception("User was not found");
+            }
+
+            bool isVerified =BCrypt.Net.BCrypt.Verify(request.Password, result.Value.Password);
+
+            if (isVerified)
+            {
+                return result.Value.Id;
+            }
+            else
+            {
+                throw new Exception("Password is incorrect");
+            }
+        }
+    }
+}
---
+++ Fitnes-Tracker-Application/Features/Users/Registration/RegisterUser.cs
@@ -0,0 +1,35 @@
+﻿using Fintess_Tracker_Application.Repository.User;
+using Fintess_Tracker_Domain.Entity;
+using MediatR;
+
+namespace Fintess_Tracker_Application.Features.Users.Registration
+{
+    public record RegisterUserCommand(string Login, string Password, string Name, DateOnly BirthDay) : IRequest<Guid>;
+
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, Guid>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public RegisterUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Guid> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            bool isLoginTaken = await _userRepository.IsLoginAlreadyTakenAsync(request.Login, cancellationToken);
+            if (isLoginTaken)
+            {
+                throw new Exception("User's login is already taken");
+            }
+
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            var user = new User(request.Login, hashedPassword, request.Name, request.BirthDay);
+
+            await _userRepository.AddNewUserAsync(user, cancellationToken);
+
+            return user.Id;
+        }
+    }
+}
