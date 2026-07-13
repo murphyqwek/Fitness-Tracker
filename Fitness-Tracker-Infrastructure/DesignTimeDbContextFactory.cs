c8e865552c14f432fe84f2362e5390b14abedd2b
murphyqwek <qwekinaft@list.ru>
2026-07-13 17:30:17 +0300

    Fix errors with cd

---
+++ Fitness-Tracker-Infrastructure/DesignTimeDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Fitness_Tracker_Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Fitness_Tracker_Infrastructure
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "Fitness-Tracker", "appsettings.json"), optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection")
+                                  ?? "Host=localhost;Database=dummy";
+
+            builder.UseNpgsql(connectionString);
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
