e0cf17c5f6c621e8379f9991cdf6cc8c9bee11fc
murphyqwek <qwekinaft@list.ru>
2026-08-25 19:06:09 +0300

    Add ProjectTo to ExerciseRepository

--- Fitness-Tracker-Application/Repository/Exercise/IExerciseRepository.cs
+++ Fitness-Tracker-Application/Repository/Exercise/IExerciseRepository.cs
@@ -1,14 +1,15 @@
-﻿using Fitness_Tracker_Domain.Entity;
+﻿using Fitness_Tracker_Application.Features.Exercise;
+using Fitness_Tracker_Domain.Entity;
 using FluentResults;
 
 namespace Fitness_Tracker_Application.Repository.Exercises
 {
     public interface IExerciseRepository
     {
-        public Task<Result<Exercise>> GetExerciseByIdAsync(int id, CancellationToken cancellationToken);
+        public Task<Result<ExerciseSearchDTO>> GetExerciseByIdAsync(int id, CancellationToken cancellationToken);
 
-        public Task<IList<Exercise>> GetExerciseAsync(string? Name, IList<int>? MuscleId, CancellationToken cancellationToken);
+        public Task<IList<ExerciseSearchDTO>> GetExerciseAsync(string? Name, IList<int>? MuscleId, CancellationToken cancellationToken);
 
-        public Task<IList<Exercise>> GetAllExerciseAsync(CancellationToken cancellationToken);
+        public Task<IList<ExerciseSearchDTO>> GetAllExerciseAsync(CancellationToken cancellationToken);
     }
 }
--- Fitness-Tracker-Infrastructure/Repository/Exercise/ExerciseRepository.cs
+++ Fitness-Tracker-Infrastructure/Repository/Exercise/ExerciseRepository.cs
@@ -1,10 +1,10 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Fitness_Tracker_Application.Features.Exercise;
 using Fitness_Tracker_Application.Repository.Exercises;
-using Fitness_Tracker_Domain.Entity;
 using Fitness_Tracker_Infrastructure.Data;
 using FluentResults;
 using Microsoft.EntityFrameworkCore;
-using System.Xml.Linq;
 
 namespace Fitness_Tracker_Infrastructure.Repository.Exercises
 {
@@ -15,58 +15,53 @@ namespace Fitness_Tracker_Infrastructure.Repository.Exercises
 
         private const double SIMILARITTY_TRESHOLD = 0.7;
 
-        public ExerciseRepository(ApplicationDbContext context, IMapper mapper) 
-        { 
+        public ExerciseRepository(ApplicationDbContext context, IMapper mapper)
+        {
             _context = context;
             _mapper = mapper;
         }
 
-        public async Task<IList<Exercise>> GetAllExerciseAsync(CancellationToken cancellationToken)
+        public async Task<IList<ExerciseSearchDTO>> GetAllExerciseAsync(CancellationToken cancellationToken)
         {
-            var result = await _context.Exercises.Include(ex => ex.Muscles)
-                            .ThenInclude(exMuscle => exMuscle.Muscle)
-                            .AsNoTracking()
-                            .ToListAsync(cancellationToken);
+            var query = _context.Exercises.ProjectTo<ExerciseSearchDTO>(_mapper.ConfigurationProvider)
+                            .AsNoTracking();
 
-            return _mapper.Map<IList<Exercise>>(result);
+            return await query.ToListAsync();
         }
 
-        public async Task<IList<Exercise>> GetExerciseAsync(string? Name, IList<int>? MusclesId, CancellationToken cancellationToken)
+        public async Task<IList<ExerciseSearchDTO>> GetExerciseAsync(string? Name, IList<int>? MusclesId, CancellationToken cancellationToken)
         {
-            var queary = _context.Exercises.Include(ex => ex.Muscles)
-                                            .ThenInclude(exMuscle => exMuscle.Muscle)
-                                            .AsNoTracking()
-                                            .AsQueryable();
+            var query = _context.Exercises.AsNoTracking();
 
-            if(MusclesId != null && MusclesId.Any()) {
-                queary = queary.Where(ex => ex.Muscles.Any(exMuscle => MusclesId.Contains(exMuscle.MuscleId)));
+            if (MusclesId != null && MusclesId.Any())
+            {
+                query = query.Where(ex => ex.Muscles.Any(exMuscle => MusclesId.Contains(exMuscle.MuscleId)));
             }
 
-            if(!string.IsNullOrEmpty(Name)) {
-                queary = queary.Where(ex =>
-                        EF.Functions.TrigramsWordSimilarityDistance(Name, ex.Name) < SIMILARITTY_TRESHOLD
-                    )
-                    .OrderBy(ex => EF.Functions.TrigramsWordSimilarityDistance(Name, ex.Name));
+            if (!string.IsNullOrEmpty(Name))
+            {
+                query = query.Where(ex => EF.Functions.TrigramsWordSimilarityDistance(Name, ex.Name) < SIMILARITTY_TRESHOLD)
+                             .OrderBy(ex => EF.Functions.TrigramsWordSimilarityDistance(Name, ex.Name));
             }
 
-            var result = await queary.ToListAsync(cancellationToken);
-
-            return _mapper.Map<IList<Exercise>>(result);
+            return await query
+                .ProjectTo<ExerciseSearchDTO>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
         }
 
-        public async Task<Result<Exercise>> GetExerciseByIdAsync(int id, CancellationToken cancellationToken)
+        public async Task<Result<ExerciseSearchDTO>> GetExerciseByIdAsync(int id, CancellationToken cancellationToken)
         {
-            var result = await _context.Exercises.Include(ex => ex.Muscles)
-                                            .ThenInclude(exMuscle => exMuscle.Muscle)
-                                            .AsNoTracking()
-                                            .FirstOrDefaultAsync(ex => ex.Id == id, cancellationToken);
+            var result = await _context.Exercises
+                        .Where(ex => ex.Id == id)
+                        .ProjectTo<ExerciseSearchDTO>(_mapper.ConfigurationProvider)
+                        .FirstOrDefaultAsync(cancellationToken);
 
             if (result == null)
             {
                 return Result.Fail($"No exercise by id: {id}");
             }
 
-            return _mapper.Map<Exercise>(result);
+            return Result.Ok(result);
         }
     }
 }
