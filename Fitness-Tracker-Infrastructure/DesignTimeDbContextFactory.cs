48d8287a2e4007350e58909455796214afb40d2f
murphyqwek <qwekinaft@list.ru>
2026-08-25 14:26:14 +0300

    Fix DbContextFactory

--- Fitness-Tracker-Infrastructure/DesignTimeDbContextFactory.cs
+++ Fitness-Tracker-Infrastructure/DesignTimeDbContextFactory.cs
@@ -9,9 +9,12 @@ namespace Fitness_Tracker_Infrastructure
     {
         public ApplicationDbContext CreateDbContext(string[] args)
         {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
             IConfigurationRoot configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile($"appsettings.{env}.json", optional: true)
                 .AddEnvironmentVariables()
                 .Build();
 
