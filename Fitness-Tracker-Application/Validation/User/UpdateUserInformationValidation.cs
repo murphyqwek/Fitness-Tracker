634164cf4de8c7e6dee97f4ad9371f80cc72e59d
murphyqwek <qwekinaft@list.ru>
2026-08-30 14:31:32 +0300

    Add Validation on update user information

---
+++ Fitness-Tracker-Application/Validation/User/UpdateUserInformationValidation.cs
@@ -0,0 +1,38 @@
+﻿using Fitness_Tracker_Application.DTO.User;
+using Fitness_Tracker_Application.Features.Users.Infomration;
+using FluentValidation;
+
+namespace Fitness_Tracker_Application.Validation.User
+{
+    public class UserInformationUpdateDTOValidation: AbstractValidator<UserUpdateDTO> 
+    {
+        public UserInformationUpdateDTOValidation()
+        {
+            RuleFor(dto => dto.name)
+                .MinimumLength(1)
+                .MaximumLength(20);
+
+            RuleFor(dto => dto.birthDay)
+                .LessThan(_ => DateOnly.FromDateTime(DateTime.Now))
+                .WithMessage("Дата рождения должна раньше, чеме сегодняшний день");
+
+            RuleFor(dto => dto.height)
+                .InclusiveBetween(0, 300);
+
+            RuleFor(dto => dto.weight)
+                .InclusiveBetween(0, 1000);
+        }
+    }
+
+    public class UpdateUserInformationValidation : AbstractValidator<UpdateUserInformationCommand>
+    {
+        public UpdateUserInformationValidation()
+        {
+            RuleFor(command => command.id)
+                .NotEmpty();
+
+            RuleFor(command => command.userUpdateDto)
+                    .SetValidator(new UserInformationUpdateDTOValidation());
+        }
+    }
+}
