3c444ac28d4820afee579d56785e1ecb40ec0635
murphyqwek <qwekinaft@list.ru>
2026-08-30 13:56:14 +0300

    Add commands on get user information and update

---
+++ Fitness-Tracker-Application/Features/Users/Infomration/GetUserInformation.cs
@@ -0,0 +1,23 @@
+﻿using Fitness_Tracker_Application.DTO.User;
+using Fitness_Tracker_Application.Repository.User;
+using FluentResults;
+using MediatR;
+
+namespace Fitness_Tracker_Application.Features.Users.Infomration
+{
+    public record GetUserInformationCommand(Guid id) : IRequest<Result<UserInformationDTO>>;
+    public class GetUserInformationCommandHandler : IRequestHandler<GetUserInformationCommand, Result<UserInformationDTO>>
+    {
+        private readonly IUserInformationRepository _repo;
+
+        public GetUserInformationCommandHandler(IUserInformationRepository repo) 
+        {
+            _repo = repo;
+        }
+
+        public async Task<Result<UserInformationDTO>> Handle(GetUserInformationCommand request, CancellationToken cancellationToken)
+        {
+            return await _repo.GetUserInformationAsync(request.id, cancellationToken);
+        }
+    }
+}
---
+++ Fitness-Tracker-Application/Features/Users/Infomration/UpdateUserInformation.cs
@@ -0,0 +1,23 @@
+﻿using Fitness_Tracker_Application.DTO.User;
+using Fitness_Tracker_Application.Repository.User;
+using FluentResults;
+using MediatR;
+
+namespace Fitness_Tracker_Application.Features.Users.Infomration
+{
+    public record UpdateUserInformationCommand(Guid id, UserUpdateDTO userUpdateDto) : IRequest<Result>;
+    public class UpdateUserInformationCommandHandler : IRequestHandler<UpdateUserInformationCommand, Result>
+    {
+        private readonly IUserInformationRepository _repo;
+
+        public UpdateUserInformationCommandHandler(IUserInformationRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Result> Handle(UpdateUserInformationCommand request, CancellationToken cancellationToken)
+        {
+            return await _repo.UpdateUserInformationAsync(request.id, request.userUpdateDto, cancellationToken);
+        }
+    }
+}
