538752d4eda062af8250f9393c4042a9410d7a06
murphyqwek <qwekinaft@list.ru>
2026-08-30 13:39:46 +0300

    Update IUserRepository contract

--- Fitness-Tracker-Application/Repository/User/IUserRepository.cs
+++ Fitness-Tracker-Application/Repository/User/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace Fitness_Tracker_Application.Repository.User
 {
     public interface IUserRepository
     {
-        public Task AddNewUserAsync(Fitness_Tracker_Domain.Entity.User user, CancellationToken cancellationToken);
+        public Task<Result> AddNewUserAsync(Fitness_Tracker_Domain.Entity.User user, CancellationToken cancellationToken);
         public Task<bool> IsLoginAlreadyTakenAsync(string login, CancellationToken cancellationToken);
         public Task<Result<Fitness_Tracker_Domain.Entity.User>> GetUserByLoginAsync(string login, CancellationToken cancellationToken);
         public Task<Result<Fitness_Tracker_Domain.Entity.User>> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken);
