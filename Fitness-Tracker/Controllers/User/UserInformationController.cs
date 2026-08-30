5a19a3522a39c755d53d94ecdb52788aa7bed671
murphyqwek <qwekinaft@list.ru>
2026-08-30 14:15:57 +0300

    Add UserInformation Controller

---
+++ Fitness-Tracker/Controllers/User/UserInformationController.cs
@@ -0,0 +1,51 @@
+﻿using Fitness_Tracker_Application.DTO.User;
+using Fitness_Tracker_Application.Features.Users.Infomration;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fitness_Tracker.Controllers.User
+{
+    [Route("api/v1/user/me")]
+    [ApiController]
+    public class UserInformationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserInformationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetUserInfo(CancellationToken cancellationToken) {
+            var id = User.GetUserId();
+
+            var result = await _mediator.Send(new GetUserInformationCommand(id), cancellationToken);
+
+            if(result.IsFailed) 
+            {
+                return NotFound(result.Errors.First().Message);
+            }
+
+            return Ok(result.Value);
+        }
+
+        [HttpPatch]
+        [Authorize]
+        public async Task<IActionResult> PatchUserInfo([FromBody] UserUpdateDTO userUpdateDTO, CancellationToken cancellationToken) 
+        {
+            var id = User.GetUserId();
+
+            var result = await _mediator.Send(new UpdateUserInformationCommand(id, userUpdateDTO), cancellationToken);
+
+            if (result.IsFailed)
+            {
+                return NotFound(result.Errors.First().Message);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ Fitness-Tracker/UserClaimsExtension.cs
@@ -0,0 +1,35 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Fitness_Tracker
+{
+    public static class UserClaimsExtension
+    {
+        public static Guid GetUserId(this ClaimsPrincipal user) 
+        {
+            var idClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value
+                   ?? user.FindFirst("sub")?.Value;
+
+            if (string.IsNullOrEmpty(idClaim) || !Guid.TryParse(idClaim, out var guid))
+            {
+                throw new UnauthorizedAccessException("Id пользователя отсутствует в токене");
+            }
+
+            return guid;
+        }
+
+        public static string GetUserLogin(this ClaimsPrincipal user)
+        {
+            var loginClaim = user.FindFirst(ClaimTypes.Name)?.Value
+                  ?? user.FindFirst(JwtRegisteredClaimNames.UniqueName)?.Value
+                  ?? user.FindFirst("unique_name")?.Value;
+
+            if (string.IsNullOrEmpty(loginClaim))
+            {
+                throw new UnauthorizedAccessException("Login пользователя отсутствует в токене");
+            }
+
+            return loginClaim;
+        }
+    }
+}
