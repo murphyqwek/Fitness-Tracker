1a4f94d5688b4197f212f01be99ba56c5a9d94f2
murphyqwek <qwekinaft@list.ru>
2026-08-30 21:45:32 +0300

    Add Workout repositories interfaces

---
+++ Fitness-Tracker-Application/Features/Workout/IWorkoutIdempotencyKeyRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Fitness_Tracker_Application.Features.Workout
+{
+    public enum IdempotencyStatus 
+    {
+        RUNNING,
+        FINISHED,
+        TAKEN,
+        UNKNOW,
+    }
+
+    public interface IWorkoutIdempotencyKeyRepository
+    {
+        public Task<(IdempotencyStatus Status, string? Result)> LockWorkout(Guid userId, Guid idempotencyKey);
+
+        public Task UpdateWorkoutIdempotencyStatus(Guid userId, Guid idempotencyKey, Guid exerciseId);
+
+        public Task DeleteWorkoutIdempotencyStatus(Guid userId, Guid idempotencyKey);
+    }
+}
---
+++ Fitness-Tracker-Application/Features/Workout/IWorkoutRepository.cs
@@ -0,0 +1,11 @@
+﻿using FluentResults;
+
+namespace Fitness_Tracker_Application.Features.Workout
+{
+    public interface IWorkoutRepository
+    {
+        public Task<Result<Guid>> CreateNewWorkout(Guid userId, CreateWorkoutDTO createWorkoutDTO);
+
+        public Task<Result<ResponseWorkoutDTO>> GetWorkoutById(Guid userId, Guid workoutId);
+    }
+}
