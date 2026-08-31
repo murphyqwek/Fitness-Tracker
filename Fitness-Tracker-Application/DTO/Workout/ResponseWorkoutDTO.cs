31769305337adbac8e78d36011fdfc009bd21520
murphyqwek <qwekinaft@list.ru>
2026-08-31 14:36:46 +0300

    Fix bug when AutoMapper couldn't parse to ResponseWorkoutDTO

--- Fitness-Tracker-Application/DTO/Workout/ResponseWorkoutDTO.cs
+++ Fitness-Tracker-Application/DTO/Workout/ResponseWorkoutDTO.cs
@@ -1,6 +1,20 @@
 ﻿namespace Fitness_Tracker_Application.DTO.Workout
 {
-    public record class ResponseWorkoutDTO(Guid Id, string Name, string Description, DateTimeOffset Date, IList<ReponseWorkoutSetDTO> workoutSets);
+    public record ResponseWorkoutDTO
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+        public DateTimeOffset Date { get; init; }
+        public IList<ReponseWorkoutSetDTO> WorkoutSets { get; init; } = [];
+    }
 
-    public record class ReponseWorkoutSetDTO(int ExerciseId, string ExerciseName, int Repetitions, decimal Weight, int Order);
+    public record ReponseWorkoutSetDTO
+    {
+        public int ExerciseId { get; init; }
+        public string ExerciseName { get; init; } = string.Empty;
+        public int Repetitions { get; init; }
+        public decimal Weight { get; init; }
+        public int Order { get; init; }
+    }
 }
