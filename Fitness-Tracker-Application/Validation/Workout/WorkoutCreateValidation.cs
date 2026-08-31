5782563d1d486a522ec55d4795134d142775f71a
murphyqwek <qwekinaft@list.ru>
2026-08-31 12:37:18 +0300

    Add validators for workout create dto

---
+++ Fitness-Tracker-Application/Validation/Workout/WorkoutCreateValidation.cs
@@ -0,0 +1,52 @@
+﻿using Fitness_Tracker_Application.DTO.Workout;
+using Fitness_Tracker_Application.Features.Workout;
+using FluentValidation;
+
+namespace Fitness_Tracker_Application.Validation.Workout
+{
+    public class WorkoutSetCreateDTOValidation : AbstractValidator<CreateWorkoutSetDTO>
+    {
+        public WorkoutSetCreateDTOValidation()
+        {
+            RuleFor(set => set.ExerciseId)
+                .GreaterThan(0);
+
+            RuleFor(set => set.Weight)
+                .GreaterThanOrEqualTo(0)
+                .LessThan(5000);
+
+            RuleFor(set => set.Repetitions)
+                .GreaterThanOrEqualTo(0)
+                .LessThan(5000);
+        }
+    }
+    
+    public class WorkoutCreateDTOValidation : AbstractValidator<CreateWorkoutDTO>
+    {
+        public WorkoutCreateDTOValidation()
+        {
+            RuleFor(dto => dto.Name)
+                .NotEmpty()
+                .MaximumLength(150);
+
+            RuleFor(dto => dto.Description)
+                .MaximumLength(500);
+
+            RuleForEach(dto => dto.workoutSets)
+                .SetValidator(new WorkoutSetCreateDTOValidation());
+        }
+    }
+
+    public class WorkoutCreateValidation : AbstractValidator<CreateWorkoutCommand>
+    {
+        public WorkoutCreateValidation()
+        {
+            RuleFor(command => command.idempotencyKey)
+                    .NotEmpty();
+
+            RuleFor(command => command.workoutDTO)
+                .NotEmpty()
+                .SetValidator(new WorkoutCreateDTOValidation());
+        }
+    }
+}
