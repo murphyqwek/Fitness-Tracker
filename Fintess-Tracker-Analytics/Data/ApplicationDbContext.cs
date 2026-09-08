2d1e08e46646741758d600603786b5dd49dc5f08
murphyqwek <qwekinaft@list.ru>
2026-09-08 10:28:57 +0300

    Add ApplicationDbContext

---
+++ Fintess-Tracker-Analytics/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Fintess_Tracker_Analytics.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fintess_Tracker_Analytics.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<WorkoutEntity> Workouts { get; set; } = null!;
+        public DbSet<WeeklyRecordEntity> WeeklyRecords { get; set; } = null!;
+        public DbSet<MonthlyVolumeEntity> MonthlyVolumes { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<WorkoutEntity>()
+                .HasKey(w => w.Id);
+            modelBuilder.Entity<WeeklyRecordEntity>()
+                .HasKey(wr => new { wr.UserId, wr.WeekStart });
+            modelBuilder.Entity<MonthlyVolumeEntity>()
+                .HasKey(mv => new { mv.UserId, mv.Year, mv.Month });
+        }
+    }
+}
