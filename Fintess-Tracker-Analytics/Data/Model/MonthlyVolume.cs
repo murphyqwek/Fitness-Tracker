8821def8cd67a4e9cabf58d9d0527cfc4b7b1664
murphyqwek <qwekinaft@list.ru>
2026-09-08 11:46:08 +0300

    Add models to analytic service

---
+++ Fintess-Tracker-Analytics/Data/Model/MonthlyVolume.cs
@@ -0,0 +1,27 @@
+﻿namespace Fintess_Tracker_Analytics.Data.Model
+{
+    public class MonthlyVolume
+    {
+        public Guid UserId { get; private set; }
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+        public decimal TotalVolume { get; private set; }
+
+        private MonthlyVolume() { }
+
+        public MonthlyVolume(Guid userId, int year, int month)
+        {
+            UserId = userId;
+            Year = year;
+            Month = month;
+        }
+
+        public void Add(decimal volume)
+        {
+            if (volume < 0)
+                throw new ArgumentOutOfRangeException(nameof(volume));
+
+            TotalVolume += volume;
+        }
+    }
+}
