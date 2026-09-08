56879b235873f0d050387e73c2b99c03443938ca
murphyqwek <qwekinaft@list.ru>
2026-09-08 11:46:48 +0300

    Add response dto and complete workout to analytic service

---
+++ Fintess-Tracker-Analytics/DTO/CompletedWorkoutDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace Fintess_Tracker_Analytics.DTO
+{
+    public sealed record CompletedWorkoutDTO(Guid WorkoutId, Guid UserId, DateTimeOffset CompletedAt, IReadOnlyCollection<CompletedSetDTO> Sets);
+
+    public sealed record CompletedSetDTO(decimal Weight, int Repetitions);
+}
