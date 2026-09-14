9e3e743db3c3546b7298152952d28b59ce166906
murphyqwek <qwekinaft@list.ru>
2026-09-14 19:19:02 +0300

    Add Background Consumer to Analytic Service

---
+++ Fintess-Tracker-Analytics/Background/Kafka/WorkoutCompletedMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Fintess_Tracker_Analytics.DTO;
+using Fitness_Tracker_Shared;
+
+namespace Fintess_Tracker_Analytics.Background.Kafka
+{
+    public class WorkoutCompletedMapper : Profile
+    {
+        public WorkoutCompletedMapper()
+        {
+            CreateMap<WorkoutCompletedV1Event, CompletedWorkoutDTO>()
+                .ForMember(dest => dest.WorkoutId, opt => opt.MapFrom(src => src.WorkoutId))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+                .ForMember(dest => dest.CompletedAt, opt => opt.MapFrom(src => src.CompletedAt))
+                .ForMember(dest => dest.Sets, opt => opt.MapFrom(src => src.Exercises.SelectMany(ex => ex.Sets)));
+        }
+    }
+}
