84b3bf6ffa68ca884436651435f07649eea1f36b
murphyqwek <qwekinaft@list.ru>
2026-09-14 18:55:26 +0300

    Add KafkaProducer

---
+++ Fitness-Tracker-Application/Service/Kafka/IKafkaProducer.cs
@@ -0,0 +1,7 @@
+﻿namespace Fitness_Tracker_Application.Service.Kafka
+{
+    public interface IKafkaProducer
+    {
+        Task Produce<T>(string topic, string key, T message, CancellationToken cancellationToken);
+    }
+}
---
+++ Fitness-Tracker-Application/Service/Kafka/KafkaProducer.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace Fitness_Tracker_Application.Service.Kafka
+{
+    public class KafkaProducer : IKafkaProducer
+    {
+        private readonly IKafkaProducer _producer;
+
+        public KafkaProducer(IKafkaProducer producer)
+        {
+            _producer = producer;
+        }
+
+        public Task Produce<T>(string topic, string key, T message, CancellationToken cancellationToken)
+        {
+            string serialized = JsonSerializer.Serialize(message);
+
+            return _producer.Produce(topic, key, serialized, cancellationToken);
+        }
+    }
+}
