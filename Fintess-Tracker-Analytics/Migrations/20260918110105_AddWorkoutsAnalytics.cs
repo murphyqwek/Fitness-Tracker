deb2af50ef21478c3e806af3921cf311a0d8addf
murphyqwek <qwekinaft@list.ru>
2026-09-18 14:01:34 +0300

    Add migration for analytics service

---
+++ Fintess-Tracker-Analytics/Migrations/20260918110105_AddWorkoutsAnalytics.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fintess_Tracker_Analytics.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorkoutsAnalytics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "analytics");
+
+            migrationBuilder.CreateTable(
+                name: "Workouts",
+                schema: "analytics",
+                columns: table => new
+                {
+                    WorkoutId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CompletedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Volume = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    MaxEstimatedOneRepMax = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workouts", x => x.WorkoutId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workouts_UserId_CompletedAt",
+                schema: "analytics",
+                table: "Workouts",
+                columns: new[] { "UserId", "CompletedAt" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Workouts",
+                schema: "analytics");
+        }
+    }
+}
